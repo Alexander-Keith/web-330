e80d1c7624a38906500b895964f2f00005d32859
Alex <alexanderkeithpro@gmail.com>
2023-05-28 17:16:06 -0500

    about to give up

--- theme.js
+++ theme.js
@@ -9,22 +9,21 @@
 
 // Make sure the reference is placed at the <head> of the HTML document. See Exhibit Q from Web 330 HTML, CSS, and JavaScript Requirements. To use this function, simply call setSelectedTheme(). Be sure to place this above all other JavaScript code. See Exhibit R.
 
-/**
- * Set the default theme to light if one has not been set in the browsers localStorage
- */
+//Function for setDefaultTheme
 function setDefaultTheme() {
+
+    //Assigns variables
     const theme = localStorage.getItem("mode") || "light-theme";
     const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
     const iconText = localStorage.getItem("iconText") || "Light Mode";
 
+    //Assigns output to HTML
     document.body.classList.value = theme;
     document.getElementById("icon-mode").classList.add(iconMode);
     document.getElementById("icon-text").innerHTML = iconText;
 }
 
-/**
- * Set the HTML body to the user's selected theme. If one has not been selected, set the theme to light-theme
- */
+//Function for setSelectedTheme
 function setSelectedTheme() {
     document.body.classList.value = localStorage.getItem("mode") || "light-theme";
 }
